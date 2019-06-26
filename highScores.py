a22521c5713fd05e3c349129388079429da8bd65
Christi Kazakov <ckazakov18@gmail.com>
2019-06-25 22:11:55 -0600

    Update getHighestScore and getLastScore

--- highScores.py
+++ highScores.py
@@ -2,3 +2,19 @@
 # 1 - Return the highest score from list
 # 2 - Return the last added score
 # 3 - Return the highest scores
+
+def getHighestScore(list):
+    max = 0
+    for x in list:
+        if x > max:
+            max = x
+
+    return list.index(max)
+
+
+def getLastScore(list):
+    return list[-1]
+
+def getTopThreeHighestScores(list):
+    # return top three scores
+    return
