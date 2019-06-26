73552b09e6e3a7bcc440635261d2e07ced898d9a
Christi Kazakov <ckazakov18@gmail.com>
2019-06-25 21:36:03 -0600

    Update twofer.py

--- twofer.py
+++ twofer.py
@@ -5,6 +5,7 @@ def checkString(user):
         print('One for you, one for me')
     else:
         print(f'One for {user}, one for me.')
+        # need to figure out args
 
 if __name__ == '__main__':
     checkString(sys.argv[1:])
